a1b403311d26db05f7d1b0a57bcad75b114b6f8c
hzmsalah <hzmsalah2@gmail.com>
2023-12-03 20:17:34 +0200

    Model, GQL Files, Nav Bar, Routing, Initiation Obj

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SDLCComponent } from './sdlc/sdlc.component';
 import { AllFilesComponent } from './all-files/all-files.component';
 import { SDLCModule } from "./sdlc/sdlc.module";
+import { FormsModule } from '@angular/forms';
 
 ;
 
+const appRoutes: Routes = [
+    {path: "", component: SDLCComponent},
+    {path: "sdlc", component: SDLCComponent},
+    {path: "all-files", component: AllFilesComponent},
+]
 @NgModule({
     declarations: [
         AppComponent,
@@ -20,7 +26,9 @@ import { SDLCModule } from "./sdlc/sdlc.module";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        SDLCModule
-    ]
+        SDLCModule,
+        RouterModule.forRoot(appRoutes),
+        FormsModule,
+    ],
 })
 export class AppModule { }
---
+++ angular-front/src/models/initiation.ts
@@ -0,0 +1,9 @@
+export class initiation {
+    title?: string;
+    startDate?:Date;
+    endDate?:Date;
+    objective?:string;
+    manager?:string;
+    budget?:number;
+    scope?:string;
+}
