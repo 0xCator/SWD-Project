727e36ac689487660ba4e255144b7cbfe548b4c4
hzmsalah <hzmsalah2@gmail.com>
2023-12-07 14:04:16 +0200

    DesignForm & All Validators Done & Works!

--- front-end/src/app/form/form.component.ts
+++ front-end/src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, FormControl} from '@angular/forms'
+import {FormBuilder, FormGroup, ReactiveFormsModule, FormControl, FormArray, Validators} from '@angular/forms'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input'
 import {MatSelectModule} from '@angular/material/select'
@@ -11,43 +11,64 @@ import { CommonModule } from '@angular/common';
 import { SharedService } from '../services/shared.service';
 
 @Component({
-  selector: 'app-form',
-  standalone:true,
-  imports: [ReactiveFormsModule, MatFormFieldModule,MatInputModule,MatSelectModule,MatButtonToggleModule,CommonModule,MatDatepickerModule,MatNativeDateModule,MatButtonModule],
-  templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+selector: 'app-form',
+standalone:true,
+imports: [ReactiveFormsModule, MatFormFieldModule,MatInputModule,MatSelectModule,MatButtonToggleModule,CommonModule,MatDatepickerModule,MatNativeDateModule,MatButtonModule],
+templateUrl: './form.component.html',
+styleUrl: './form.component.css'
 })
 
 
 export class FormComponent implements OnInit{
-  InitiationForm!: FormGroup;
-  SRSForm!: FormGroup;
-  DesignForm!: FormGroup;
-  phase: string =''
-  constructor(private formBuilder: FormBuilder, private sc:SharedService){}
-  ngOnInit(): void {
-    this.InitiationForm = this.formBuilder.group({
-      title:'',
-      startDate:null,
-      endDate:null,
-      objectives:'',
-      manager:'',
-      budget:'',
-      scope:'',
+InitiationForm!: FormGroup;
+SRSForm!: FormGroup;
+DesignForm!: FormGroup;
+phase: string =''
+constructor(private formBuilder: FormBuilder, private sc:SharedService){}
+ngOnInit(): void {
+  this.InitiationForm = this.formBuilder.group({
+    title:['', Validators.required],
+    startDate:[null, Validators.required],
+    endDate:[null, Validators.required],
+    objectives:['', Validators.required],
+    manager:['', Validators.required],
+    budget:['', Validators.required],
+    scope:['', Validators.required],
+  })
+  this.SRSForm = this.formBuilder.group({
+    introduction:['', Validators.required],
+    purpose:['', Validators.required],
+    audience:['', Validators.required],
+    description:['', Validators.required],
+    featuresAndRequirements:['', Validators.required],
+    useCase:[null, Validators.required],
+  })
+
+  this.DesignForm = this.formBuilder.group({
+    files: new FormArray([])
     })
-    this.SRSForm = this.formBuilder.group({
-      introduction:'',
-      purpose:'',
-      audience:'',
-      description:'',
-      featuresAndRequirements:'',
-      useCase:null,
+  }
+  
+  get designForms(){
+    return(this.DesignForm.controls['files']) as FormArray;
+  }
+  
+  addDesignForm(){
+    let document = new FormGroup({
+      filename: new FormControl('', Validators.required),
+      imagefile: new FormControl('', Validators.required)
     })
+
+    this.designForms.push(document);
   }
-  handleButtonClick(buttonValue: string) {
-    this.phase = buttonValue;
+  
+  removeDesignForm(i: number){
+    this.designForms.removeAt(i);
   }
-  save(){
-    this.sc.updateSharedVariable(false)
-  }
+handleButtonClick(buttonValue: string) {
+  this.phase = buttonValue;
+}
+save(){
+  this.sc.updateSharedVariable(false)
+}
 }
